77791fdbc4c870b6a1172926fb7b2719779363f9
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-09 13:31:21 +0100

    inserts new post into DB

--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,5 @@
+import postModel from "../models/postModel.js";
+
 // GET /posts
 function getPosts(req, res) {
     res.json({
@@ -5,6 +7,33 @@ function getPosts(req, res) {
     })
 }
 
+// POST /posts/new -> creates new article
+async function postNewPost(req, res) {
+    // data for new post
+    const newPost = req.body;
+
+    try {
+        // insert into DB
+        await postModel.createPost(newPost);
+
+        res.status(200).json({ newPost });        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
 export default {
     getPosts,
+    postNewPost,
 }
+
+/* 
+post = {
+    title
+    content
+    created (date)
+    published (bool)
+    authorId
+}
+*/
--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,6 @@ import postsController from "../controllers/post.js";
 const postRouter = Router();
 
 postRouter.get("/", postsController.getPosts);
+postRouter.post("/new", postsController.postNewPost)
 
 export default postRouter;
