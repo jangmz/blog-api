02386e0af4cbbf8c2bad74bbc00683b23014785a
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-11 14:54:57 +0100

    implemented token verification middleware

--- routes/post.js
+++ routes/post.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import postsController from "../controllers/post.js";
+import { verifyAuthorToken, verifyUserToken } from "../middleware/tokenMiddleware.js";
 
 const postRouter = Router();
 
 postRouter.get("/", postsController.getPosts);
-postRouter.get("/:postId", postsController.getPost);
-postRouter.post("/", postsController.postNewPost)
-postRouter.delete("/:postId", postsController.deletePost);
-postRouter.put("/:postId", postsController.updatePost);
-postRouter.put("/publish/:postId", postsController.publishPost);
-postRouter.put("/unpublish/:postId", postsController.unpublishPost);
+postRouter.get("/:postId", verifyUserToken, postsController.getPost);
+postRouter.post("/", verifyAuthorToken, postsController.postNewPost)
+postRouter.delete("/:postId", verifyAuthorToken, postsController.deletePost);
+postRouter.put("/:postId", verifyAuthorToken,postsController.updatePost);
+postRouter.put("/publish/:postId", verifyAuthorToken,postsController.publishPost);
+postRouter.put("/unpublish/:postId", verifyAuthorToken,postsController.unpublishPost);
 
 export default postRouter;
