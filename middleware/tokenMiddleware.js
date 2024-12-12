0bab5979cecace60ff21fded2899563f325d817c
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-12 09:57:27 +0100

    updated req.user variable

--- middleware/tokenMiddleware.js
+++ middleware/tokenMiddleware.js
@@ -21,7 +21,7 @@ export function verifyAuthorToken(req, res, next) {
 
         req.user = authData.user;
 
-        //console.log(req.user);
+        console.log(req.user);
 
         if (req.user.role === "AUTHOR") {
             next();
@@ -49,7 +49,7 @@ export function verifyUserToken(req, res, next) {
             return res.status(403).json({ message: "Forbidden access." });
         }
 
-        req.user = authData;
+        req.user = authData.user;
 
         if (req.user.role === "USER" || "AUTHOR") {
             next();
